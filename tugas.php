6b77574398ac199ce00f87a511e36861e7f48f8e
rikaanrokhim <rikaanrokhim1@gmail.com>
2017-10-06 18:51:35 +0700

    tugas

---
+++ tugas.php
@@ -0,0 +1,197 @@
+<?php
+echo "<font face='Calibri'>";
+echo "Perkenalkan nama saya Rika An Rokhim, saat ini saya sedang magang di PT.Dhezign Online Solution sebagai siswa PKL. Brikut adalah Biodata Pribadi saya";
+echo "<br>";
+echo "<hr>";
+echo "<center>";
+echo"<h2>";
+echo "<b>";
+echo "Biodata Pribadi";
+echo "<b>";
+echo"<h2>";
+echo "<center>";
+echo "<hr>";
+
+echo "<table width='600' border='0'>";
+echo "<tr>";
+	echo "<td width='51'>";
+	echo "&nbsp;";
+	echo "</td>";
+
+	echo "<td width='150'>";
+	echo "Nama Lengkap";
+	echo "</td>";
+
+	echo "<td width='5'>";
+	echo ":";
+	echo "</td>";
+
+	echo "<td width='200'>";
+	echo "Rika An Rokhim";
+	echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+	echo "<td width='51'>";
+	echo "&nbsp;";
+	echo "</td>";
+
+	echo "<td width='150'>";
+	echo "Nama Panggilan";
+	echo "</td>";
+
+	echo "<td width='5'>";
+	echo ":";
+	echo "</td>";
+
+	echo "<td width='200'>";
+	echo "Rika";
+	echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+	echo "<td width='51'>";
+	echo "&nbsp;";
+	echo "</td>";
+
+	echo "<td width='150'>";
+	echo "Tempat, Tanggal Lahir";
+	echo "</td>";
+
+	echo "<td width='5'>";
+	echo ":";
+	echo "</td>";
+
+	echo "<td width='200'>";
+	echo "Ponorogo, 26 April 2001";
+	echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+	echo "<td width='51'>";
+	echo "&nbsp;";
+	echo "</td>";
+
+	echo "<td width='150'>";
+	echo "Umur";
+	echo "</td>";
+
+	echo "<td width='5'>";
+	echo ":";
+	echo "</td>";
+
+	echo "<td width='200'>";
+	echo "16 tahun";
+	echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+	echo "<td width='51'>";
+	echo "&nbsp;";
+	echo "</td>";
+
+	echo "<td width='150'>";
+	echo "Alamat Rumah";
+	echo "</td>";
+
+	echo "<td width='5'>";
+	echo ":";
+	echo "</td>";
+
+	echo "<td width='200'>";
+	echo "Jl. Padepokan No. 07, Ponorogo, Jawa Timur";
+	echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+	echo "<td width='51'>";
+	echo "&nbsp;";
+	echo "</td>";
+
+	echo "<td width='150'>";
+	echo "Agama";
+	echo "</td>";
+
+	echo "<td width='5'>";
+	echo ":";
+	echo "</td>";
+
+	echo "<td width='200'>";
+	echo "Islam";
+	echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+	echo "<td width='51'>";
+	echo "&nbsp;";
+	echo "</td>";
+
+	echo "<td width='150'>";
+	echo "Nama Ayah";
+	echo "</td>";
+
+	echo "<td width='5'>";
+	echo ":";
+	echo "</td>";
+
+	echo "<td width='200'>";
+	echo "Sukardi";
+	echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+	echo "<td width='51'>";
+	echo "&nbsp;";
+	echo "</td>";
+
+	echo "<td width='150'>";
+	echo "Nama Ibu";
+	echo "</td>";
+
+	echo "<td width='5'>";
+	echo ":";
+	echo "</td>";
+
+	echo "<td width='200'>";
+	echo "Sri Rahayu";
+	echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+	echo "<td width='51'>";
+	echo "&nbsp;";
+	echo "</td>";
+
+	echo "<td width='150'>";
+	echo "Asal Sekolah";
+	echo "</td>";
+
+	echo "<td width='5'>";
+	echo ":";
+	echo "</td>";
+
+	echo "<td width='200'>";
+	echo "SMKN 1 Ponorogo";
+	echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+	echo "<td width='51'>";
+	echo "&nbsp;";
+	echo "</td>";
+
+	echo "<td width='150'>";
+	echo "Kelas/Jurusan";
+	echo "</td>";
+
+	echo "<td width='5'>";
+	echo ":";
+	echo "</td>";
+
+	echo "<td width='200'>";
+	echo "XI/RPL";
+	echo "</td>";
+echo "</tr>";
+echo "</table>";
+echo "<hr>";
+?>
